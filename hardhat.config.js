f3110923f890894ebf4d4f91f1776b020c515054
gongyexiangjin <717054907@qq.com>
2025-01-13 23:13:32 +0800

    feat: 网络&私钥配置

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  // defaultNetwork: "hardhat", // 默认网络，可以不写
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      // 可以从第三方拿到免费的JSON-RPC URL Alchemy/Infura/QuickNode
+      url: 'https://eth-sepolia.g.alchemy.com/v2/UGo-zdgfEKl3oq6rN6HhtxwVUW4UKpgr', // https://dashboard.alchemy.com/apps
+      // 私钥地址，是一个数组。MetaMask或者其他钱包的账户中获取
+      accounts: ["6d8f5e03675b0313c9abf10881397e901cd48c3eff7199e593414854f7d1638d"],
+    }
+  }
 };
